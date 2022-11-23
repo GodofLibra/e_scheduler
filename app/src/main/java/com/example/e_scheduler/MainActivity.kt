8693fe2b978aaa962993c1820819ae0b901bf847
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2022-11-23 21:34:21 +0530

    Remove bottom nav bar

--- app/src/main/java/com/example/e_scheduler/MainActivity.kt
+++ app/src/main/java/com/example/e_scheduler/MainActivity.kt
@@ -52,7 +52,6 @@ class MainActivity : AppCompatActivity() {
             )
         ).setOpenableLayout(drawerLayout).build()
         setupActionBarWithNavController(navController, appBarConfiguration)
-        bottom_nav_bar.setupWithNavController(navController)
         navView.setupWithNavController(navController)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
         NavigationUI.setupWithNavController(navView, navController)
@@ -60,11 +59,9 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id in listOf(R.id.loginActivity, R.id.signUpActivity, R.id.additionalProfileFragment)) {
-                bottom_nav_bar.visibility = View.GONE
                 toolbar.visibility = View.GONE
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
             } else {
-                bottom_nav_bar.visibility = View.VISIBLE
                 toolbar.visibility = View.VISIBLE
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
             }
